f39fe5ba3cee87135e888b113f3a252648393b87
Artefilex <baris.tncdmr@gmail.com>
2024-03-14 05:36:27 +0300

    first version finish change audio added

--- src/Pages/Question.jsx
+++ src/Pages/Question.jsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { setSingleData } from "../store/DataSlice";
+import { setMatched, setSingleData, setVisible } from "../store/DataSlice";
+import success from "../assets/success.mp3";
+import wah from "../assets/lose.mp3";
 function Question() {
-  const { singelData } = useSelector((state) => state.DataReducer);
+  const { singelData, matched } = useSelector((state) => state.DataReducer);
+  const [showAll, setShowAll] = useState(false);
   const [value, setValue] = useState("");
 
   const { id } = useParams();
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
-    const fetchData = setInterval(() => {
-      dispatch(setSingleData(Number(id) + 1));
-    }, 200);
-    
-    return () => clearInterval(fetchData);
+    dispatch(setSingleData(Number(id) + 1));
   }, [id, dispatch]);
- 
 
   const handleClick = (e) => {
     e.preventDefault();
-    const result = singelData.options.filter((q) => {
-        return q.title.toLowerCase().includes(value.toLowerCase());
-      });
-    console.log(result);
-    console.log(result);
+    if (value) {
+      dispatch(setVisible(value));
+      setTimeout(() => {
+        dispatch(setMatched());
+      }, 1000);
+    }
     setValue("");
   };
 
   return (
     <div className="flex flex-col w-full items-center bg-[color:var(--main-color)] text-white font-bold h-[100vh]">
       <div className="flex gap-3 items-center justify-center mt-4">
+        {matched === true ? (
+          <audio src={success} autoPlay></audio>
+        ) : matched === false ? (
+          <audio src={wah} autoPlay></audio>
+        ) : null}
         <input
           className=" border-none outline-none text-black px-4 py-1 "
           type="text"
@@ -41,31 +45,53 @@ function Question() {
           className="flex items-center justify-center bg-slate-600 px-2 py-1"
           onClick={handleClick}
         >
-          {" "}
           Search
         </button>
+        <button onClick={() => setShowAll(!showAll)}>Show All</button>
       </div>
-      <header className="flex flex-col item-center justify-center mt-10">
-        {singelData && singelData.question}
-      </header>
-      <main>
-        {singelData &&
-          singelData.options &&
-          singelData.options.map((option, i) => (
-            <div key={i} className="w-full flex items-center justify-between ">
-              {option.visibel ? (
-                <div>
-                  {option.title} - {option.score}
+
+      {showAll ? (
+        <section className="flex flex-col w-full items-center bg-[color:var(--main-color)] text-white font-bold">
+        <header className="mt-10 text-2xl">
+            {singelData && singelData.question}
+          </header>
+          <main className="flex flex-col items-center justify-center mt-10 gap-1">
+            {singelData &&
+              singelData.options &&
+              singelData.options.map((option, i) => (
+                <div key={i} className="w-[25rem] flex  items-center justify-between text-xl  ">
+                    <span>{option.title}</span> <span> {option.score} PUAN </span>    
                 </div>
-              ) : (
-                <div className="w-[25rem] h-10  bg-slate-700 my-3" />
-              )}
-            </div>
-          ))}
-      </main>
-      <button>
-        <Link to={`/question/${Number(id)+ 1 }`}> Diğer Soruya Geç</Link>
-      </button>
+              ))}
+          </main>
+          <button>
+            <Link to={`/question/${Number(id) + 1}`}> Diğer Soruya Geç</Link>
+          </button>
+        </section>
+      ) : (
+        <section className="flex flex-col w-full items-center bg-[color:var(--main-color)] text-white font-bold">
+          <header className=" mt-10 text-2xl">
+            {singelData && singelData.question}
+          </header>
+          <main className="flex flex-col items-center justify-center mt-10 gap-1">
+            {singelData &&
+              singelData.options &&
+              singelData.options.map((option, i) => (
+                <div key={i} className="w-[25rem] flex  items-center justify-between px-3  text-xl">
+                  {option.visibel ? (
+                    <>
+                   <span> {option.title}</span>   <span> {option.score} PUAN </span>  
+                    </>
+                  ) : (
+                    <div className="w-[25rem] h-10  bg-slate-700 my-3 animate-pulse" />
+                  )}
+                </div>
+              ))}
+               {singelData &&
+              singelData.options &&   singelData.options.every((item) =>item.visibel === true) &&  <Link to={`/question/${Number(id) + 1}`}> Diğer Soruya Geç</Link> } 
+          </main>
+        </section>
+      )}
     </div>
   );
 }
--- src/store/DataSlice.js
+++ src/store/DataSlice.js
@@ -3,19 +3,52 @@ import { Ask_Data } from "../mock/Data";
 
 const initialState = {
   askData: Ask_Data,
-  singelData: null
+  singelData: null,
+  matched: null,
 };
 
 const questionSlice = createSlice({
   name: "questionSlicer",
   initialState,
   reducers: {
-     setSingleData : (state, action) => {
-         state.singelData = state.askData.find((item) => item.id === action.payload)
+    setSingleData: (state, action) => {
+      state.singelData = state.askData.find(
+        (item) => item.id === action.payload
+      );
+    },
+    setVisible: (state, action) => {
+      const finddata = state.singelData.options.find((item) => {
+        return item.title.toLowerCase().includes(action.payload.toLowerCase());
+      });
+      if (!finddata) {
+        state.matched = false;
+     
+      } else {
+        state.matched = true;
+      }
+      const updatedOptions = state.singelData.options.map((item) => {
+        const doesMatch = item.title
+          .toLowerCase()
+          .includes(action.payload.toLowerCase());
+
+        return {
+          ...item,
+          visibel: doesMatch ? true : item.visibel,
+        };
+      });
+
+      state.singelData = {
+        ...state.singelData,
+        options: updatedOptions,
+      };
+    
+    },
+    setMatched : (state) => {
+      state.matched = null
     }
   },
 });
 
-export const {setSingleData} = questionSlice.actions
+export const { setSingleData, setVisible ,setMatched } = questionSlice.actions;
 
-export default questionSlice.reducer
+export default questionSlice.reducer;
